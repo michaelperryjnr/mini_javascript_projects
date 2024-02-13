8b178ca4eac42074ea717e97276fb6f1eb89b24d
michaelperryjnr <michaelperryt97@gmail.com>
2024-02-13 01:30:49 +0000

    Added index.js

---
+++ digital clock/index.js
@@ -0,0 +1,18 @@
+// let counters = 0;
+
+// function loadCount() {
+//   counters++;
+//   return counters;
+// }
+
+// setInterval(loadCount, 1000);
+
+// let pageWindow = window;
+// function loadPage() {
+//   if (counters % 2 == 0) {
+//     pageWindow.load();
+//   }
+// }
+
+
+// loadPage()
