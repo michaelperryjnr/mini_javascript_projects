0715945393736119a4d8749f4ade7c96c7317189
michaelperryjnr <michaelperryt97@gmail.com>
2024-02-23 04:26:06 +0000

    Added try catch for error handling

--- weather-forecast/weather.js
+++ weather-forecast/weather.js
@@ -9,19 +9,25 @@ const weatherDiv = document.getElementById('weather-div');
 
 const url = `https://wttr.in/${location}?format=%C+%t+%w`;
 const loader = document.getElementById('loader-container');
-
+const errorDiv = document.getElementById('error');
+const loaderAnim = document.getElementById('loader');
 
 submit.addEventListener('click', () => {
   const location = document.getElementById('location').value || 'accra';
   const url = `https://wttr.in/${location}`;
   weatherDiv.innerHTML = '';
   loader.style.display = 'contents';
-  fetch(url)
+  try {
+     fetch(url)
     .then((response) => response.text())
     .then((data) => {
       loader.style.display = 'none';
       weatherDiv.innerHTML = data;
       console.log(data);
     });
-
+  } catch (error) {
+    loaderAnim.style.display = 'none';
+    console.log(error);
+    errorDiv.innerHTML = 'An error occured when fetching the weather data. Please try again.';
+  }
 });
