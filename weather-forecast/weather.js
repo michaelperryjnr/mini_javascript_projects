63c262e51d168c40e73368cb5374d3907f8bb2c2
michaelperryjnr <michaelperryt97@gmail.com>
2024-02-23 04:17:40 +0000

    Added fetch and loader

--- weather-forecast/weather.js
+++ weather-forecast/weather.js
@@ -1,5 +1,27 @@
-const form = document.getElementById('submit');
+const submit = document.getElementById('submit');
 
-form.addEventListener('click', () => {
-  document.body.innerHTML = ''
+submit.preventDefault =(event) => {
+  console.log('preventDefault');
+  return false;
+};
+
+const weatherDiv = document.getElementById('weather-div');
+
+const url = `https://wttr.in/${location}?format=%C+%t+%w`;
+const loader = document.getElementById('loader-container');
+
+
+submit.addEventListener('click', () => {
+  const location = document.getElementById('location').value || 'accra';
+  const url = `https://wttr.in/${location}`;
+  weatherDiv.innerHTML = '';
+  loader.style.display = 'contents';
+  fetch(url)
+    .then((response) => response.text())
+    .then((data) => {
+      loader.style.display = 'none';
+      weatherDiv.innerHTML = data;
+      console.log(data);
+    });
+
 });
