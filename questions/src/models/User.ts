07ed0655cd9905b7e1802570c94f97677a878682
michaelperryjnr <michaelperryt97@gmail.com>
2024-08-07 15:30:38 +0000

    Time stamping

--- questions/src/models/User.ts
+++ questions/src/models/User.ts
@@ -1,10 +1,15 @@
 import { IUser } from "@/interfaces";
 import mongoose, { Schema } from "mongoose";
 
-const UserSchema = new Schema({
-  username: { type: String, required: true },
-  scores: { type: Array, required: true },
-});
+const UserSchema = new Schema(
+  {
+    username: { type: String, required: true },
+    scores: { type: Array, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 console.log("Checking existing models:", mongoose.models);
 const BBFUser =
