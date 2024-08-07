7de52043e50a732b49ed9f9e446358c3a351a45a
michaelperryjnr <michaelperryt97@gmail.com>
2024-08-07 17:34:23 +0000

    Refactor gh pages

--- dcit202/tracker.js
+++ dcit202/tracker.js
@@ -1,45 +1,45 @@
-const baseurl = "https://bbfquestions.vercel.app/api";
-const METHODS = ["POST", "PUT"];
+// const baseurl = "https://bbfquestions.vercel.app/api";
+// const METHODS = ["POST", "PUT"];
 
-const username = localStorage.getItem("username") || "anonymous";
-const scores = JSON.parse(localStorage.getItem("scores")) || [];
+// const username = localStorage.getItem("username") || "anonymous";
+// const scores = JSON.parse(localStorage.getItem("scores")) || [];
 
-async function updateDetailsToAPIOnReload() {
-  try {
-    const response = await fetch(`${baseurl}/users`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, scores }),
-      mode: "cors",
-    });
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-}
+// async function updateDetailsToAPIOnReload() {
+//   try {
+//     const response = await fetch(`${baseurl}/users`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ username, scores }),
+//       mode: "cors",
+//     });
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-updateDetailsToAPIOnReload();
+// updateDetailsToAPIOnReload();
 
-function updateDetailsToAPI() {
-  try {
-    const response = fetch(`${baseurl}/users`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, scores }),
-      mode: "cors",
-    });
-    console.log(response);
-  } catch (error) {
-    console.log(error);
-  }
-}
+// function updateDetailsToAPI() {
+//   try {
+//     const response = fetch(`${baseurl}/users`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ username, scores }),
+//       mode: "cors",
+//     });
+//     console.log(response);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-updateDetailsToAPI();
+// updateDetailsToAPI();
 
-//fire on windowclose
-window.addEventListener("beforeunload", updateDetailsToAPIOnReload);
+// //fire on windowclose
+// window.addEventListener("beforeunload", updateDetailsToAPIOnReload);
