11c0118f05b783876db3c124e8605678d23a4c68
michaelperryjnr <michaelperryt97@gmail.com>
2024-08-07 15:52:05 +0000

    Finished Tracker

--- dcit202/tracker.js
+++ dcit202/tracker.js
@@ -1,6 +1,40 @@
 const baseurl = "https://bbfquestions.vercel.app/api";
+const METHODS = ["POST", "PUT"];
 
 const username = localStorage.getItem("username") || "anonymous";
 const scores = JSON.parse(localStorage.getItem("scores")) || [];
 
-async function updateDetailsOnReload() {}
+async function updateDetailsToAPIOnReload() {
+  try {
+    const response = await fetch(`${baseurl}/users`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, scores }),
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+updateDetailsToAPIOnReload();
+
+function updateDetailsToAPI() {
+  try {
+    const response = fetch(`${baseurl}/users`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, scores }),
+    });
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+updateDetailsToAPI();
