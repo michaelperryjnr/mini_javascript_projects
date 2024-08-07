21b717d2a534b6699f4083a6eff29918840ae6d1
Michael Perry Tettey <michaelperryt97@gmail.com>
2024-08-07 13:50:34 +0000

    Update controllers

--- questions/src/app/api/questions/route.ts
+++ questions/src/app/api/questions/route.ts
@@ -3,16 +3,16 @@ import Questions from "@/models/Question";
 import { User } from "@/controllers";
 import { connectToDb } from "@/db";
 
-
 export async function GET(request: Request) {
   await connectToDb();
   const questions = await Questions.find();
+  console.log(questions);
   return NextResponse.json({ questions });
 }
 
 export async function POST(request: Request) {
   await connectToDb();
   const body = await request.json();
-  const newQuestion = User.addQuestion(body)
+  const newQuestion = User.addQuestion(body);
   return NextResponse.json({ message: "Question created successfully" });
 }
